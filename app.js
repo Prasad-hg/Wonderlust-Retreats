aeea313f1dae3b2e03f449f82a1dc3f5380d14c5
Prasad-hg <prasadhg2003@gmail.com>
2025-04-16 09:11:08 +0530

    Add Projectl Files

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
+if(process.env.NODE_ENV != "production"){
+  require('dotenv').config()
+
+}
+
+console.log(process.env)
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
 const Review = require('./models/review.js');
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require('ejs-mate');
-const wrapAsync = require('./utils/wrapAsync.js');
 const ExpressError = require('./utils/ExpressError.js');
-const {listingSchema} = require('./schema.js');
-const {reviewSchema} = require('./schema.js');
-// const Review = require('./models/review.js');
+const session = require("express-session");
+const MongoStore = require('connect-mongo');
+const flash = require("connect-flash");
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user.js');
+// const {reviewSchema} = require('./schema.js');
+const listingRouter = require('./routes/listing.js');
+const reviewRouter = require('./routes/review.js');
+const userRouter = require('./routes/user.js');
+
 
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "/public")))
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLASDB_URL;
 
+
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  crypto:{
+    secret: process.env.SECRET,
+  },
+  touchAfter:24*3600,
+});
+
+store.on("error", ()=>{
+  console.log("error in mongo session");
+})
 main()
   .then(() => {
     console.log("connected to DB");
@@ -25,7 +50,7 @@ main()
   });
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  await mongoose.connect(dbUrl);
 }
 
 app.set("view engine", "ejs");
@@ -33,113 +58,43 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
-app.get("/", (req, res) => {
-  res.send("Hi, I am root");
-});
+const sessionOptions = {
+  store,
+  secret : process.env.SECRET,
+  resave : false,
+  saveUninitialized : true,
+  cookie : {
+    expires : Date.now() * 7 * 24 * 60 * 60 *1000,
+    maxAge: 7 * 24 * 60 * 60 *1000,
+    httpOnly: true,
+  },
 
-const validateListing = (req, res, next) => {
-  let { error} = listingSchema.validate(req.body);
-  if (error) {
-    let errMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(400, errMsg);
-  } else {
-  next();
-  }
 };
+app.use(session(sessionOptions));
+app.use(flash());
 
-const validateReview = (req, res, next) => {
-  let { error} =reviewSchema.validate(req.body);
-  if (error) {
-    let errMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(400, errMsg);
-  } else {
-  next();
-  }
-};
-//Index Route
-app.get("/listings", wrapAsync(async (req, res) => {
-  const allListings = await Listing.find({});
-  res.render("listings/index.ejs", { allListings });
-}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
 
-//New Route
-app.get("/listings/new", (req, res) => {
-  res.render("listings/new.ejs");
-});
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-//Show Route
-app.get("/listings/:id", wrapAsync(async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id).populate("reviews");
-  res.render("listings/show.ejs", { listing });
-}));
-
-//Create Route
-app.post("/listings", validateListing,wrapAsync(async (req, res, next) => {
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
-  
-}));
-
-//Edit Route
-app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id);
-  res.render("listings/edit.ejs", { listing });
-}));
-
-//Update Route
-app.put("/listings/:id",validateListing,wrapAsync( async (req, res) => {
-  let { id } = req.params;
-  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-  res.redirect(`/listings/${id}`);
-}));
-
-//Delete Route
-app.delete("/listings/:id", wrapAsync(async (req, res) => {
-  let { id } = req.params;
-  let deletedListing = await Listing.findByIdAndDelete(id);
-  console.log(deletedListing);
-  res.redirect("/listings");
-}));
-
-
-// Reviews
-// Post route
-app.post('/listings/:id/reviews',validateReview,wrapAsync(async (req,res)=>{
-  let listing  = await Listing.findById(req.params.id);
-  let newReview = new Review(req.body.review);
-
-  listing.reviews.push(newReview);
-
-  await newReview.save();
-  await listing.save();
-
-  console.log("new review saved",newReview);
-  res.redirect(`/listings/${listing._id}`);
-}));
-
-// Delete Reviews
-app.post('/listings/:id/reviews/:reviewId', wrapAsync(async(req,res)=>{
-  let {id, reviewId} = req.params;
-  await Listing.findByIdAndUpdate(id,{$pull: {reviews:reviewId}});
-  await Review.findByIdAndDelete(reviewId);
-  res.redirect(`/listings/${id}`);
-}))
-// app.get("/testListing", async (req, res) => {
-//   let sampleListing = new Listing({
-//     title: "My New Villa",
-//     description: "By the beach",
-//     price: 1200,
-//     location: "Calangute, Goa",
-//     country: "India",
-//   });
-
-//   await sampleListing.save();
-//   console.log("sample was saved");
-//   res.send("successful testing");
+// app.get("/", (req, res) => {
+//   res.send("Hi, I am root");
 // });
+
+app.use((req,res,next)=>{
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currUser = req.user;
+  next();
+});
+
+app.use('/listings', listingRouter);
+app.use('/listings/:id/reviews', reviewRouter);
+app.use('/', userRouter);
 
 app.all("*",(req,res,next)=>{
   next(new ExpressError(404,"Page not found!"));
--- init/index.js
+++ init/index.js
@@ -18,6 +18,7 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) =>({...obj, owner: "67f8f6d973c9cd181c14080a"} ));
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
