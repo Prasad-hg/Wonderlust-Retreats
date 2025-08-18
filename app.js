69e9cc37a02e98e965c09beaaad6801d32c19714
Prasad Hg <141731531+Prasad-hg@users.noreply.github.com>
2025-08-18 23:52:31 +0530

    app update

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ const sessionOptions = {
   resave : false,
   saveUninitialized : true,
   cookie : {
-    expires : Date.now() * 7 * 24 * 60 * 60 *1000,
+    expires : Date.now() + 7 * 24 * 60 * 60 *1000,
     maxAge: 7 * 24 * 60 * 60 *1000,
     httpOnly: true,
   },
